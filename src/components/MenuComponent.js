82849b210d44780849f9893a8ae0f1f250995060
Tahir Bin Yousuf <7132757@gmail.com>
2020-08-22 04:34:54 -0700

    one

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -15,23 +15,24 @@ class Menu extends Component {
   onDishSelect(dish) {
     this.setState({ selectedDish: dish});
   }
-  render() {
-      return(
-    {menu = this.props.dishes.map(dish => {
-      return (
-        <div key={dish.id} className="col-12 col-md-5 m-1">
-          <Card onClick={() => this.onDishSelect(dish)}>
-            <CardImg width="100%" src={dish.image} alt={dish.name} />
-            <CardImgOverlay className="ml-5">
-              <CardTitle>{dish.name}</CardTitle>
-            </CardImgOverlay> 
-          </Card>
-        </div>
-      );
-    })
-    }
-    )
-  }
+  render() 
+  // {
+  //     return(
+  //   {this.props.dishes.map(dish => {
+  //     return (
+  //       <div key={dish.id} className="col-12 col-md-5 m-1">
+  //         <Card onClick={() => this.onDishSelect(dish)}>
+  //           <CardImg width="100%" src={dish.image} alt={dish.name} />
+  //           <CardImgOverlay className="ml-5">
+  //             <CardTitle>{dish.name}</CardTitle>
+  //           </CardImgOverlay> 
+  //         </Card>
+  //       </div>
+  //     );
+  //   })
+  //   }
+  //   )
+  // }
 }
 
 export default Menu;
